f5a5ea52f4c8772ed6068e92befe31af8223d49d
Cormick <cvhnilicka@wisc.edu>
2017-01-23 10:43:58 -0600

    removed use strict

--- app/app.js
+++ app/app.js
@@ -1,11 +1,10 @@
-'use strict';
+// 'use strict';
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  // 'myApp.view3',
   'myApp.appointmentsView',
   'myApp.version',
   'myApp.dayViews'
