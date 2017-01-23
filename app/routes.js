9accdbf544d9b1ddca5cd1a7419be387301ecd63
Cormick <cvhnilicka@wisc.edu>
2017-01-23 10:37:11 -0600

    removed mongoose models

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,3 @@
-var Appointment = require('./models/appointment');
-var Blackout = require('./models/blackout')
 var mysql = require('mysql');
 var nodemailer = require('nodemailer');
 var times = ['8:00','8:30','9:00','9:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00',
